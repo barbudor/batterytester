92a991a352b21d645338a61685366a4b3f2c3283
Barbudor <barbudor@barbudor.net>
2019-03-21 00:08:38 +0100

    initial commit

--- README.md
+++ README.md
@@ -2,31 +2,21 @@
 Introduction
 ============
 
-CircuitPython driver for the Texas Instruments' INA3221 3 channels current sensor.
-`Product page <http://www.ti.com/product/INA3221>`_.
-
-2 version are available:
-
-* normal/full		includes all constants for low-level register access + API for alarms
-* lite				only basic API and no constants - use less memory
+Batteries-tester for up to 3 batteries (typically Li-* batteries). The circuite uses a INA3221 triple current
+sensor to measure batteries voltage and current drawn during a discharge cycle. Measrumentss are logged into
+the file system as CSV files which can then be imported in Excel in order to draw graph or perform additional
+calculations.
 
 Dependencies
 =============
-This driver depends on:
+This script depends on:
 
 * `Adafruit CircuitPython <https://github.com/adafruit/circuitpython>`_
 * `Bus Device <https://github.com/adafruit/AdafruitCircuitPythonBusDevice>`_
-
-Please ensure all dependencies are available on the CircuitPython filesystem.
-This is easily achieved by downloading
-`the Adafruit library and driver bundle <https://github.com/adafruit/AdafruitCircuitPythonBundle>`_.
-
-Usage Example
-=============
-
-see `example <https://github.com/barbudor/CircuitPythonINA3221/blob/master/examples/ina3221simpletest.py>`_.
+* Barbudor's `CircuitPython_INA3221 driver <https://github.com/barbudor/CircuitPython_INA3221>`_
 
 Contributing
 ============
 
-Contributions welcomed. My goal is to keep the library simple and as low on memory as possible.
+This is a simple test I've done for my own usage for which is not worth contribution.
+But feel free to fork, copy and adapt for your own usage.
--- batterytester.py
+++ batterytester.py
@@ -1,244 +1,254 @@
-#--------------------------------------------------------
-#    Li-Ion Battery Tester by barbudor
-#-----------------------------------------------------------
-# Allows to test capacity of Li-Ion batteries in gang
-# Based on
-#   - AdafruitCircuit Playground Express running CPy 4.0.0b3
-#   - INA3221 for voltage/current measurement
-#   - 4 relays board (Chinese, such as SainSmart)
-#--------------------------------------------------------
+"""
+`batterytester`
+============================================================
+Li-Ion Battery Tester by barbudor
+------------------------------------------------------------
+Allows to test capacity of Li-Ion batteries in //
+Based on
+- AdafruitCircuit Playground Express running CPy 4.0.0beta
+- INA3221 for voltage/current measurement
+- 4 relays board (Chinese, such as SainSmart)
+"""
 
-import digitalio
-from board import *
-from adafruit_circuitplayground.express import cpx
-import storage
 import time
 import os
+import storage
+from micropython import const
+import digitalio
+import board
+from adafruit_circuitplayground.express import cpx
+
+from barbudor_ina3221_lite import INA3221
+
 
 class Tester:
+    """battery tester state-machine"""
 
-  # states
-  STATE_WAITING = 0
-  STATE_RUNNING = 2
-  STATE_RUNNING_FAST = 4
-  STATE_ENDING = 8
-  STATE_ENDED = 9
+    # pylint: disable=bad-whitespace
+    # states
+    _STATE_WAITING            = const(0)
+    _STATE_RUNNING            = const(2)
+    _STATE_RUNNING_FAST       = const(4)
+    _STATE_ENDING             = const(8)
+    _STATE_ENDED              = const(9)
 
-  # CPX pin labels for relays
-  RELAY_ON = False
-  RELAY_OFF = True
-  
-  # CPX neopixel index
-  PIX_WAITING = (0,0,85)
-  PIX_RUNNING = (0,85,0)
-  PIX_RUNNING_FAST = (10,85,0)
-  PIX_ENDING = (85,85,0)
-  PIX_ENDED = (255,0,0)
-  PIX_OFF = (0,0,0)
-  
-  # INA3221 resistor value
-  SHUNT_VALUE = 0.1
-  
-  FILE_COUNTER = "/tester.count"
-  FILE_LOG = "/battery%03d.csv"
-  LOG_HEADER = "File: %s\nTime; Voltage (V); Current (A)\n"
-  LOG_FORMAT = "%9.2f; %6.3f; %6.3f\n"
+    # CPX pin labels for relays
+    _RELAY_ON                 = const(False)
+    _RELAY_OFF                = const(True)
 
-  SAMPLE_PERIOD_DEFAULT = 10.0
-  SAMPLE_PERIOD_FAST = 1.0
-  SAMPLE_PERIOD_ENDING = 0.5
-  ENDING_DURATION = 60
-  
-  END_VOLTAGE = 3.0
-  
-  
-  def setpix(self,color):
-    cpx.pixels[self.pixel] = color
-    
-  def getpix(self):
-    return cpx.pixels[self.pixel]
-    
-  def readFileCounter(self):
-    counter = 0
-    try:
-      with open( Tester.FILE_COUNTER, "r" ) as file:
-        line = file.readline()
-        line = line.strip()
-        counter = int(line)
-      print("%9.2f:[%d]: read counter %d" % (time.monotonic(), self.channel, counter) )
-    except:
-      print("%9.2f:[%d]: read counter default %d" % (time.monotonic(), self.channel, counter) )
-      pass
-    return counter
-    
-    
-  def writeFileCounter(self,counter):
-    print("%9.2f:[%d]: write counter %d" % (time.monotonic(), self.channel, counter) )
-    try:
-      with open( Tester.FILE_COUNTER, "w" ) as file:
-        file.write( "%d\n" % counter )
-    except:
-      pass
-      
-      
-  def readVA_sim(self):
-    voltage = 4.200
-    current = 0.000
-    if self.relay.value == Tester.RELAY_ON:
-      if self.previousVoltage < 3.4:
-        voltage = 4.000-(self.sampleCount/(10.0+self.channel))
-        current = 1.123-(self.sampleCount/(40.0+self.channel))
-      else:
-        voltage = 4.000-(self.sampleCount/(20.0+self.channel))
-        current = 1.123-(self.sampleCount/(40.0+self.channel))
-    self.sampleCount += 1
-    print("%9.2f:[%d]: %5.3fV %5.3fA" % (time.monotonic(), self.channel, voltage, current) )
-    return ( voltage, current )
+    # CPX neopixel index
+    _PIX_WAITING = (0, 0, 85)
+    _PIX_RUNNING = (0, 85, 0)
+    _PIX_RUNNING_FAST = (10, 85, 0)
+    _PIX_ENDING = (85, 85, 0)
+    _PIX_ENDED = (255, 0, 0)
+    _PIX_OFF = (0, 0, 0)
 
-  def readVA(self):
-    return self.readVA_sim()
-    
-    
-  def writeLog(self,timestamp,voltage,current):
-    with open( self.logfilename, "a" ) as file:
-      file.write( Tester.LOG_FORMAT % ( timestamp, voltage, current ) )
+    # INA3221 resistor value
+    _SHUNT_VALUE              = const(0.1)
 
-    
-  def __init__(self, channel, pin, neopixel):
-    self.relay = digitalio.DigitalInOut(pin)
-    self.relay.switch_to_output(value=Tester.RELAY_OFF)
-    self.channel = channel
-    self.pixel = neopixel
-    self.state = Tester.STATE_WAITING
-    self.setpix(Tester.PIX_WAITING)
+    _FILE_COUNTER             = "/tester.count"
+    _FILE_LOG                 = "/battery%03d.csv"
+    _LOG_HEADER               = "File: %s\nTime; Voltage (V); Current (A)\n"
+    _LOG_FORMAT               = "%9.2f; %6.3f; %6.3f\n"
 
-    
-  def __del__(self):
-    self.deinit()
+    _SAMPLE_PERIOD_DEFAULT    = const(10.0)
+    _SAMPLE_PERIOD_FAST       = const(1.0)
+    _SAMPLE_PERIOD_ENDING     = const(0.5)
+    _ENDING_DURATION          = const(60)
 
-    
-  def deinit(self):
-    self.state = Tester.STATE_WAITING
-    self.setpix(Tester.PIX_WAITING)
-    self.relay.value = Tester.RELAY_OFF
-    
-  def start(self):
-    self.state = Tester.STATE_RUNNING
-    pixel = Tester.PIX_RUNNING
-    self.setpix(tuple(3*x for x in pixel))
+    _END_VOLTAGE              = const(3.0)
+    # pylint: enable=bad-whitespace
 
-    filecount = self.readFileCounter()
-    self.logfilename = Tester.FILE_LOG % filecount
-    with open( self.logfilename, "w" ) as file:
-      file.write( Tester.LOG_HEADER % self.logfilename )
-    self.writeFileCounter(filecount+1)
-    self.sampleCount = 0
-    self.samplePeriod = Tester.SAMPLE_PERIOD_DEFAULT
-    self.ending = Tester.ENDING_DURATION
-    
-    print("%9.2f:[%d]: starting '%s'" % (time.monotonic(), self.channel, self.logfilename) )
-    
-    # initial sample performed with relay off (battery not loaded)
-    self.startTime = time.monotonic()
-    (voltage,current) = self.readVA()
-    self.writeLog( 0, voltage, current )
+    def _setpix(self, color):
+        cpx.pixels[self.pixel] = color
 
-    # switch on the load relay
-    self.relay.value = Tester.RELAY_ON
-    
-    self.previousVoltage = voltage
-    self.previousDeltaV = 5.0
-    # 1st sample after switching on the load is done just 1 sec after
-    self.nextTime = self.startTime + 1.0
+    def _getpix(self):
+        return cpx.pixels[self.pixel]
 
-    self.setpix(pixel)
+    def _read_file_counter(self):
+        counter = 0
+        try:
+            with open(Tester._FILE_COUNTER, "r") as file:
+                line = file.readline()
+                line = line.strip()
+                counter = int(line)
+                print("%9.2f:[%d]: read counter %d" % (time.monotonic(), self.channel, counter))
+        except:
+            print("%9.2f:[%d]: read counter default %d" % (time.monotonic(), self.channel, counter))
+            #pass
+        return counter
+
+    def _write_file_counter(self, counter):
+        print("%9.2f:[%d]: write counter %d" % (time.monotonic(), self.channel, counter))
+        try:
+            with open(Tester._FILE_COUNTER, "w") as file:
+                file.write("%d\n" % counter)
+        except:
+            pass
+
+    def _read_v_and_i_sim(self):
+        voltage = 4.200
+        current = 0.000
+        if self.relay.value == Tester._RELAY_ON:
+            if self.previous_voltage < 3.4:
+                voltage = 4.000-(self.sample_count/(10.0+self.channel))
+                current = 1.123-(self.sample_count/(40.0+self.channel))
+            else:
+                voltage = 4.000-(self.sample_count/(20.0+self.channel))
+                current = 1.123-(self.sample_count/(40.0+self.channel))
+        self.sample_count += 1
+        print("%9.2f:[%d]: %5.3fV %5.3fA" % (time.monotonic(), self.channel, voltage, current))
+        return (voltage, current)
+
+    def _read_v_and_i(self):
+        #return self._read_v_and_i_sim()
+        voltage = self.sensor.bus_voltage(self.channel)
+        current = self.sensor.current(self.channel)
+        return (voltage, current)
 
 
-  def run(self):
-    if self.state < Tester.STATE_ENDED:
-      now = time.monotonic()
-      if now >= self.nextTime:
-        print("%9.2f:[%d]: sample" % (time.monotonic(), self.channel) )
-        pixel = self.getpix()
-        self.setpix(tuple(3*x for x in pixel))
-        (voltage,current) = self.readVA()
-        self.writeLog( now - self.startTime, voltage, current )
+    def _write_log(self, timestamp, voltage, current):
+        with open(self.logfilename, "a") as file:
+            file.write(Tester._LOG_FORMAT % (timestamp, voltage, current))
 
-        deltaV = self.previousVoltage - voltage
-        print("%9.2f:[%d]: dV=%f pdV=%f" % (time.monotonic(), self.channel, deltaV, self.previousDeltaV) )
-        if self.state < Tester.STATE_RUNNING_FAST:
-          if deltaV >= (1.5 * self.previousDeltaV):
-            print("%9.2f:[%d]: ->run_fast" % (time.monotonic(), self.channel) )
-            self.state = Tester.STATE_RUNNING_FAST
-            pixel = Tester.PIX_RUNNING_FAST
-            self.samplePeriod = Tester.SAMPLE_PERIOD_FAST
-          self.previousDeltaV = deltaV
-          self.previousVoltage = voltage
-        
-        if self.state < Tester.STATE_ENDING:
-          if voltage <= Tester.END_VOLTAGE:
-            print("%9.2f:[%d]: ->ending" % (time.monotonic(), self.channel) )
-            self.relay.value = Tester.RELAY_OFF
-            self.state = Tester.STATE_ENDING
-            pixel = Tester.PIX_ENDING
-            self.samplePeriod = Tester.SAMPLE_PERIOD_ENDING
-        
-        if self.state == Tester.STATE_ENDING:
-          self.ending -= 1
-          if self.ending == 0:
-            print("%9.2f:[%d]: ended" % (time.monotonic(), self.channel) )
-            self.state = Tester.STATE_ENDED
-            pixel = Tester.PIX_ENDED
-            
-        self.setpix(pixel)
-        self.nextTime += self.samplePeriod
+    def __init__(self, sensor, channel, pin, neopixel):
+        self.relay = digitalio.DigitalInOut(pin)
+        self.relay.switch_to_output(value=Tester._RELAY_OFF)
+        self.sensor = sensor
+        self.channel = channel
+        self.pixel = neopixel
+        self.sample_count = 0
+        self.sample_period = 0
+        self.ending = 0
+        self.state = Tester._STATE_WAITING
+        self._setpix(Tester._PIX_WAITING)
+
+    def __del__(self):
+        self.deinit()
+
+    def deinit(self):
+        self.state = Tester._STATE_WAITING
+        self._setpix(Tester._PIX_WAITING)
+        self.relay.value = Tester._RELAY_OFF
+
+    def start(self):
+        # initialise state machine
+        self.sample_count = 0
+        self.sample_period = Tester._SAMPLE_PERIOD_DEFAULT
+        self.ending = Tester._ENDING_DURATION
+        self.state = Tester._STATE_RUNNING
+        pixel = Tester._PIX_RUNNING
+        self._setpix(tuple(3*x for x in pixel))
+        # create log file
+        filecount = self._read_file_counter()
+        self.logfilename = Tester._FILE_LOG % filecount
+        with open(self.logfilename, "w") as file:
+            file.write(Tester._LOG_HEADER % self.logfilename)
+        self._write_file_counter(filecount+1)
+        print("%9.2f:[%d]: starting '%s'" % (time.monotonic(), self.channel, self.logfilename))
+        # initial sample performed with relay off (battery not loaded)
+        self.start_time = time.monotonic()
+        (voltage, current) = self._read_v_and_i()
+        self._write_log(0, voltage, current)
+        # switch on the load relay
+        self.relay.value = Tester._RELAY_ON
+        # initialise some values
+        self.previous_voltage = voltage
+        self.previous_delta_v = 5.0
+        # 1st sample after switching on the load is done just 1 sec after
+        self.next_time = self.start_time + 1.0
+        # restore pixel
+        self._setpix(pixel)
+
+    def run(self):
+        if self.state < Tester._STATE_ENDED:
+            now = time.monotonic()
+            if now >= self.next_time:
+                print("%9.2f:[%d]: sample" % (time.monotonic(), self.channel))
+                # save current pixel then increase light
+                pixel = self._getpix()
+                self._setpix(tuple(3*x for x in pixel))
+                (voltage, current) = self._read_v_and_i()
+                self._write_log(now - self.start_time, voltage, current)
+
+        delta_v = self.previous_voltage - voltage
+        print("%9.2f:[%d]: dV=%f pdV=%f" % \
+            (time.monotonic(), self.channel, delta_v, self.previous_delta_v))
+        if self.state < Tester._STATE_RUNNING_FAST:
+            if delta_v >= (1.5 * self.previous_delta_v):
+                print("%9.2f:[%d]: ->run_fast" % (time.monotonic(), self.channel))
+                self.state = Tester._STATE_RUNNING_FAST
+                pixel = Tester._PIX_RUNNING_FAST
+                self.sample_period = Tester._SAMPLE_PERIOD_FAST
+            self.previous_delta_v = delta_v
+            self.previous_voltage = voltage
+
+        if self.state < Tester._STATE_ENDING:
+            if voltage <= Tester._END_VOLTAGE:
+                print("%9.2f:[%d]: ->ending" % (time.monotonic(), self.channel))
+                self.relay.value = Tester._RELAY_OFF
+                self.state = Tester._STATE_ENDING
+                pixel = Tester._PIX_ENDING
+                self.sample_period = Tester._SAMPLE_PERIOD_ENDING
+
+        if self.state == Tester._STATE_ENDING:
+            self.ending -= 1
+            if self.ending == 0:
+                print("%9.2f:[%d]: ended" % (time.monotonic(), self.channel))
+                self.state = Tester._STATE_ENDED
+                pixel = Tester._PIX_ENDED
+
+        # restore pixel or update to new
+        self._setpix(pixel)
+        self.next_time += self.sample_period
 
 
 ################################################################################
 
 cpx.pixels.brightness = 0.1
 
-# create testers
-testers = (Tester(0, A1, 6), Tester(1, A2, 7), Tester(2, A3, 8))
-#testers = [Tester(0, A1, 6)]
+# create measure chip
+i2c_bus = board.I2C()
+ina = INA3221(i2c_bus)
 
+# create testers
+testers = (Tester(1, ina, board.A1, 6), Tester(2, ina, board.A2, 7), Tester(3, ina, board.A3, 8))
+#testers = [Tester(1, ina, board.A1, 6)]
 
 def RunTest():
-  
-  # check if FS is writable
-  try:
-    with open( "/test.test", "w") as f:
-      f.write("tested\n")
-    os.remove( "/test.test" )
-  except:
+    # check if FS is writable
     try:
-      storage.remount("/", False)
+        with open("/test.test", "w") as f:
+            f.write("tested\n")
+        os.remove("/test.test")
     except:
-      cpx.pixels[0] = (255,0,0,)
-      while True:
-        pass
+        try:
+            storage.remount("/", False)
+        except:
+            cpx.pixels[0] = (255, 0, 0,)
+            while True:
+                pass
 
-  # wait press A to start
-  while not cpx.button_a:
-    cpx.red_led = not cpx.red_led
-    time.sleep(0.200)
-  cpx.red_led = False
-  
-  # start state-machines
-  for t in testers:
-    t.start()
+    # wait press A to start
+    while not cpx.button_a:
+        cpx.red_led = not cpx.red_led
+        time.sleep(0.200)
+        cpx.red_led = False
 
-  # test loop, press B to exit
-  while not cpx.button_b:
+    # start state-machines
     for t in testers:
-      t.run()
+        t.start()
 
-  # clean-up
-  for t in testers:
-    t.deinit()
+    # test loop, press B to exit
+    while not cpx.button_b:
+        for t in testers:
+            t.run()
+
+    # clean-up
+    for t in testers:
+        t.deinit()
 
 ################################################################################
-   
+
 #RunTest()
-
-
